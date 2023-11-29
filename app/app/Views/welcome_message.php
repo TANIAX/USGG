63146191aa03dbe80b4bd4472e3abc45cf6db613
Guillaume <35234027+TANIAX@users.noreply.github.com>
2023-11-30 00:28:36 +0100

    timeline complete

--- app/app/Views/welcome_message.php
+++ app/app/Views/welcome_message.php
@@ -1,5 +1,6 @@
 <?= $this->extend('default') ?>
-<?= $this->section('page_title') ?>Acceuil<?= $this->endSection() ?>
+<?= $this->section('page_title') ?>Acceuil
+<?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
 
@@ -12,118 +13,171 @@
 </header>
 
 <!-- Timeline -->
-<div class="bg-gray-100 py-8 sm:py-12 flex justify-center">
-  <div class="flex" style="min-width:50%;">
-    <div class="flex flex-col w-full">
-      <h1 class="mt-10 text-4xl font-bold tracking-tighter italic	uppercase mx-auto sm:text-6xl">Garçons</h1>
-      <div class="flow-root mt-4 p-8">
-        <ul role="list" class="-mb-8 flex flex-col items-center">
-          <li class="p-4 flex justify-between pt-24 pb-12" style="max-width:350px;">
-          <img src="<?= base_url('assets/img/logo-baladins.png') ?>" class="w-20 mx-4 rounded-full">
-            <div class="flex flex-col items-center justify-center">
-              <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight ">6 à 8 ans</label>
-              <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Baladins</span>
-            </div>
-          </li>
+<article class="bg-slate-100 pt-12">
+  <div class="container mx-auto max-w-[1000px]">
+    <div class="hidden xl:block w-24 mt-[730px] ml-[-100px] absolute">
+      <h1 class="-rotate-90 uppercase text-6xl font-bold p-2">Garçons</h1>
+    </div>
+    <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-6">
+      <div class="md:col-span-3 p-4 reveal-reverse">
+        <img src="<?= base_url('assets/img/logo-scout.png') ?>" class="p-4 mx-auto max-w-[500px] max-h-[200px]"
+          alt="logo scouts">
 
+        <div class="flex justify-center md:justify-normal md:flex-row-reverse w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-baladins.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom rotate-4 shadow-md" alt="logo baladins">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">6 à 8 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">Baladins</span>
+          </div>
+        </div>
 
-          <li class="p-4 flex justify-between pb-12" style="max-width:350px;">
-          <img src="<?= base_url('assets/img/logo-louveteaux.png') ?>" class="w-20 mx-4 rounded-full">
-            <div class="flex flex-col items-center justify-center">
-            <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight">8 à 12 ans</label>
-            <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Louvetaux</span>
-            </div>
-          </li>
+        <hr>
 
-          <li class="p-4 flex justify-between pb-12" style="max-width:350px;">
-          <img src="<?= base_url('assets/img/logo-eclaireurs.png') ?>" class="w-20 mx-4 rounded-full">
-            <div class="flex flex-col items-center justify-center">
-            <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight">12 à 16 ans</label>
-            <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Éclaireurs</span>
-            </div>
-          </li>
+        <div class="flex justify-center md:justify-normal md:flex-row-reverse w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-louveteaux.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom rotate-4 shadow-md" alt="logo louvetaux">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">8 à 12 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">Louvetaux</span>
+          </div>
+        </div>
 
-          <li class="p-4 flex justify-between" style="max-width:350px;">
-          <img src="<?= base_url('assets/img/logo-pionniers.png') ?>" class="w-20 mx-4 rounded-full">
-            <div class="flex flex-col items-center justify-center">
-            <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight mx-auto">16 à 18 ans</label>
-            <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Pionners</span>
-            </div>
-          </li>
-        </ul>
+        <hr>
+
+        <div class="flex justify-center md:justify-normal md:flex-row-reverse w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-eclaireurs.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom rotate-4 shadow-md" alt="logo éclaireurs">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">12 à 16 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">Éclaireurs</span>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="flex justify-center md:justify-normal md:flex-row-reverse w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-pionniers.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom rotate-4 shadow-md" alt="logo pionners">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">16 à 18 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">Pionners</span>
+          </div>
+        </div>
       </div>
+
+      <div class="hidden xl:block w-24 mt-[587px] ml-[1000px] absolute">
+        <h1 class="rotate-90 uppercase text-6xl font-bold p-2">Fille</h1>
+      </div>
+      <div class="md:col-span-3 reveal">
+        <img src="<?= base_url('assets/img/logo-guide.png') ?>" class="p-4 mx-auto max-w-[500px] max-h-[200px] mt-4"
+          alt="logo guide">
+
+          <div class="flex justify-center md:justify-normal flex-row-reverse md:flex-row w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-nutons.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom -rotate-4 shadow-md" alt="logo nuton">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">5 à 7 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">Nutons</span>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="flex justify-center md:justify-normal flex-row-reverse md:flex-row w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-lutins.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom -rotate-4 shadow-md" alt="logo lutin">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">8 à 11 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">lutins</span>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="flex justify-center md:justify-normal flex-row-reverse md:flex-row w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-aventures.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom -rotate-4 shadow-md" alt="logo aventures">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">11 à 15 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">Aventures</span>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="flex justify-center md:justify-normal flex-row-reverse md:flex-row w-full py-8">
+          <div class="bg-white p-4 w-32 h-40 flex items-center justify-center rounded-lg shadow-md transition-all ease-in-out magnetic hover:w-40">
+            <img src="<?= base_url('assets/img/logo-horizons.png') ?>"
+              class="w-28 h-28 bg-gray-100 origin-bottom -rotate-4 shadow-md" alt="logo horizons">
+          </div>
+          <div class="flex flex-col items-center justify-center p-8 magnetic">
+            <label class="font-bold text-4xl tracking-tighter uppercase leading-tight hover:text-indigo-600">16 à 18 ans</label>
+            <span class="font-bold text-gray-500 text-2xl tracking-tigh italic uppercase">Horizons</span>
+          </div>
+        </div>
+      </div>   
     </div>
 
-    <div class="flex flex-col w-full">
-    <h1 class="mt-10 text-4xl font-bold tracking-tighter italic	uppercase mx-auto sm:text-6xl">Filles</h1>
-          <div class="flow-root mt-4 p-8">
-        <ul role="list" class="-mb-8 flex flex-col items-center ml-4">
-          <li class="p-4 flex justify-between pb-12" style="max-width:350px;">
-            <div class="flex flex-col items-center justify-center">
-              <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight">5 à 7 ans</label>
-              <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Nuton</span>
-            </div>
-            <img src="<?= base_url('assets/img/logo-nutons.png') ?>" class="w-20 mx-4 rounded-full">
-          </li>
-
-
-          <li class="p-4 flex justify-between pb-12" style="max-width:350px;">
-            <div class="flex flex-col items-center justify-center">
-            <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight">7 à 11 ans</label>
-            <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Lutin</span>
-            </div>
-            <img src="<?= base_url('assets/img/logo-lutins.png') ?>" class="w-20 mx-4 rounded-full">
-          </li>
-
-          <li class="p-4 flex justify-between pb-12" style="max-width:350px;">
-            <div class="flex flex-col items-center justify-center">
-            <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight">11 à 15 ans</label>
-            <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Aventure</span>
-            </div>
-            <img src="<?= base_url('assets/img/logo-aventures.png') ?>" class="w-20 mx-4 rounded-full">
-          </li>
-
-          <li class="p-4 flex justify-between" style="max-width:350px;">
-            <div class="flex flex-col items-center justify-center">
-            <label class="font-bold text-2xl tracking-tighter italic uppercase leading-tight">15 à 17 ans</label>
-            <span class="font-bold text-gray-500 tracking-tigh italic uppercase">Horizon</span>
-            </div>
-            <img src="<?= base_url('assets/img/logo-horizons.png') ?>" class="w-20 mx-4 rounded-full">
-          </li>
-        </ul>
-      </div>
-    </div>
   </div>
-</div>
+
+</article>
+
+
+
 <!-- News -->
 <div class="bg-white py-8 sm:py-12">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl text-center">
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Actualités</h2>
-      <p class="mt-2 text-lg leading-8 text-gray-600">En ce moment chez les scouts et guides de <span class="font-bold">Gosselies</span>.</p>
+      <p class="mt-2 text-lg leading-8 text-gray-600">En ce moment chez les scouts et guides de <span
+          class="font-bold">Gosselies</span>.</p>
     </div>
     <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 
-      <?php if (!empty($news) && is_array($news)) : ?>
-        <?php foreach ($news as $key => $item) : ?>
-          <article class="flex flex-col items-start justify-between animate__animated <?= getAnimateClass($key) ?> animate__slow animate__delay-1s">
+      <?php if (!empty($news) && is_array($news)): ?>
+        <?php foreach ($news as $key => $item): ?>
+          <article
+            class="flex flex-col items-start justify-between animate__animated <?= getAnimateClass($key) ?> animate__slow animate__delay-1s">
             <div class="relative w-full">
               <img src="<?= $item->picture ?>">
               <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
             </div>
             <div class="max-w-xl">
               <div class="mt-8 flex items-center gap-x-4 text-xs">
-                <p class="text-gray-500"><?= $item->created_at->format('d/m/Y') ?></time>
-                  <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"><?= $item->category->name ?></a>
+                <p class="text-gray-500">
+                  <?= $item->created_at->format('d/m/Y') ?></time>
+                  <a href="#"
+                    class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+                    <?= $item->category->name ?>
+                  </a>
               </div>
               <div class="group relative">
-                <h3 class="mt-3 text-lg text-ellipsis whitespace-nowrap overflow-hidden font-semibold leading-6 text-gray-900 group-hover:text-gray-600" style="max-width: 350px;">
+                <h3
+                  class="mt-3 text-lg text-ellipsis whitespace-nowrap overflow-hidden font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+                  style="max-width: 350px;">
                   <a href="#">
                     <span class="absolute inset-0"></span>
                     <?= $item->title ?>
                   </a>
                 </h3>
-                <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?= $item->content ?></p>
+                <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                  <?= $item->content ?>
+                </p>
               </div>
               <div class="relative mt-8 flex items-center gap-x-4">
                 <img src="<?= $item->author->picture ?>" alt="" class="h-10 w-10 rounded-full bg-gray-100">
@@ -134,21 +188,26 @@
                       <?= $item->author->totem ?>
                     </a>
                   </p>
-                  <p class="text-sm font-semibold leading-6 text-indigo-600"><?= $item->author->user_type->name ?></p>
+                  <p class="text-sm font-semibold leading-6 text-indigo-600">
+                    <?= $item->author->user_type->name ?>
+                  </p>
                 </div>
               </div>
             </div>
           </article>
         <?php endforeach ?>
-      <?php else : ?>
+      <?php else: ?>
         <p>Aucune actualité pour le moment</p>
       <?php endif ?>
 
     </div>
   </div>
   <!-- More Post -->
-  <div id="more_post" class="relative flex flex-col items-center justify-center overflow-hidden py-12 sm:py-24 bg-white">
-    <button id="more_post_button" type="button" class="rounded-md px-6 py-2.5 tracking-widest uppercase text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-50 reveal">voir plus d'actualités</button>
+  <div id="more_post"
+    class="relative flex flex-col items-center justify-center overflow-hidden py-12 sm:py-24 bg-white">
+    <button id="more_post_button" type="button"
+      class="rounded-md px-6 py-2.5 tracking-widest uppercase text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-50 reveal">voir
+      plus d'actualités</button>
   </div>
 </div>
 
@@ -158,19 +217,24 @@
 <div class="bg-slate-100 py-24 sm:py-32">
   <div class="mx-auto max-w-7xl">
     <div id="team_header" class="mx-auto px-6 lg:px-8 animate__animated animate__slow">
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Portrait des chefs</h2>
-      <!-- <p class="mt-6 text-lg leading-8 text-gray-600">Les unités possèdes chacuns leurs propres chefs.</p> -->
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Portrait des responsables</h2>
+      <!-- <p class="mt-6 text-lg leading-8 text-gray-600">Les unités possèdes chacuns leurs propres responsables.</p> -->
     </div>
-    <ul id="team_list" role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6 reveal">
-      <?php if (!empty($users) && is_array($users)) : ?>
-        <?php foreach ($users as $user) : ?>
+    <ul id="team_list" role="list"
+      class="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6 reveal">
+      <?php if (!empty($users) && is_array($users)): ?>
+        <?php foreach ($users as $user): ?>
           <li>
             <img class="mx-auto h-24 w-24 rounded-full" src="<?= $user->picture ?>" alt="">
-            <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900"><?= $user->totem ?></h3>
-            <p class="text-sm font-semibold leading-6 text-indigo-600"><?= $user->user_type ?></p>
+            <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">
+              <?= $user->totem ?>
+            </h3>
+            <p class="text-sm font-semibold leading-6 text-indigo-600">
+              <?= $user->user_type ?>
+            </p>
           </li>
         <?php endforeach ?>
-      <?php else : ?>
+      <?php else: ?>
         <li>
           <p class="text-sm font-semibold leading-6 text-indigo-600">Aucun chef pour le moment</p>
         </li>
@@ -231,10 +295,19 @@
     text-shadow: 0 0 .3rem black;
     font-weight: 600;
   }
+
+  .rotate-4 {
+    transform: rotate(-4deg);
+  }
+
+  .-rotate-4 {
+    transform: rotate(4deg);
+  }
 </style>
 
+<script type="text/javascript" src="<?= base_url('assets/js/TweenMax.2.1.3.min.js')?>"></script>
 <script>
-  window.onload = function() {
+  window.onload = function () {
     document.body.className += ' loaded'
   };
 </script>
--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -4,7 +4,7 @@
  * @returns {void}
  */
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+    var reveals = document.querySelectorAll(".reveal , .reveal-left , .reveal-right , .reveal-reverse");
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
@@ -17,5 +17,30 @@ function reveal() {
     }
 }
 
+/**
+ * Moves the magnet button based on the mouse position.
+ * @param {Event} event - The mouse event object.
+ */
+function moveMagnet(event) {
+    var magnetButton = event.currentTarget
+    var bounding = magnetButton.getBoundingClientRect()
+  
+    TweenMax.to( magnetButton, 1, {
+      x: ((( event.clientX - bounding.left)/magnetButton.offsetWidth) - 0.5) * strength,
+      y: ((( event.clientY - bounding.top)/magnetButton.offsetHeight) - 0.5) * strength,
+      ease: Power4.easeOut
+    })
+  }
+
+
+
 window.addEventListener("scroll", reveal);
 reveal();
+var magnets = document.querySelectorAll('.magnetic')
+var strength = 50
+magnets.forEach( (magnet) => {
+  magnet.addEventListener('mousemove', moveMagnet );
+  magnet.addEventListener('mouseout', function(event) {
+    TweenMax.to( event.currentTarget, 1, {x: 0, y: 0, ease: Power4.easeOut})
+  } );
+});
